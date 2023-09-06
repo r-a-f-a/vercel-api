919622c642422a98a936ffaf7fe126eb6d520a35
Rafael Rodrigues <rrodrigues@allin.com.br>
2023-09-06 09:54:42 -0300

    [API] CHANGE MESSAGES

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+  res.send('Chupa Lucas! 🥳');
+});
 
 app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+  res.send('Rota também funciona ');
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
